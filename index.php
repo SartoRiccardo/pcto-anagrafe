ddfbf1e085d870df4a298c842611292186cd60d2
SartoRiccardo <rikki.sarto@gmail.com>
2020-01-27 20:38:13 +0100

    Added react bases

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PCTO</title>
+
+    <!-- Load React. -->
+    <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+    <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+
+    <!-- React components -->
+    <script type="text/babel" src="src/components/ui/App.js"></script>
+    <script type="text/babel" src="src/components/forms/Login.js"></script>
+    <script type="text/babel" src="src/index.js"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
---
+++ src/components/forms/Login.js
@@ -0,0 +1,32 @@
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: "",
+      pswd: "",
+    };
+  }
+
+  submitHandler = evt => {
+    evt.preventDefault();
+  }
+
+  changeHandler = evt => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  }
+
+  render() {
+    const {user, pswd} = this.state;
+    return(
+      <form onSubmit={this.submitHandler}>
+        <input type="text" name="user" onChange={this.changeHandler} value={user} />
+        <input type="password" name="pswd" onChange={this.changeHandler} value={pswd} />
+        <input type="submit" value="Login"/>
+      </form>
+    )
+  }
+}
