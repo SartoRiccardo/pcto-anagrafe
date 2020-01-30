f79e3fe79eac3018a6b50248434fce9dc557bd73
SartoRiccardo <rikki.sarto@gmail.com>
2020-01-30 14:28:32 +0100

    Fixed imports

--- api/company/index.php
+++ api/company/index.php
@@ -3,9 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
 include "./get.php";
-include "./post.php";
-include "./put.php";
-include "./delete.php";
 
 switch ($_SERVER["REQUEST_METHOD"]) {
   case "GET":
