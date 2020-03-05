bc8729df11008f0055b4ff9ebd5cd5a1ed41619e
SartoRiccardo <rikki.sarto@gmail.com>
2020-03-05 14:54:25 +0100

    Hacky user api but im gonna migrate to the framework anyways

---
+++ api/user/index.php
@@ -0,0 +1,39 @@
+<?php
+include "../config/authconfig.php";
+include "../database/database.php";
+include "../auth/privileges.php";
+include "../auth/get.php";
+
+header("Access-Control-Allow-Origin: $cors");
+header('Content-Type: application/json');
+
+if(!isset($_POST["user"])) {
+  echo json_encode(array(
+    "error" => true,
+    "message" => "Blocked anonymous request."
+  ));
+  die();
+}
+$user = intval($_POST["user"]);
+
+switch ($_POST["REQUEST_METHOD"]) {
+  case "GET":
+    if(!hasPermission($user, "ADMIN")) {
+      echo json_encode(array(
+        "error" => true,
+        "message" => "The given user does not have ADMIN permissions."
+      ));
+      die();
+    }
+
+    if(isset($_POST["id"]) && is_numeric($_POST["id"])) {
+      echo json_encode(
+        getUserById(intval($_POST["id"]))
+      );
+    }
+    die();
+
+  default:
+    // Send 404
+    break;
+}
