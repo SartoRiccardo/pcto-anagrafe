cfddf90cb51b72e3b345a7d078f6275e492c1903
SartoRiccardo <rikki.sarto@gmail.com>
2020-04-12 23:10:32 +0200

    shut up eslint

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import {Provider} from "react-redux";
 import rootReducer from "./redux/reducers/rootReducer";
 import thunk from "redux-thunk";
 
+// eslint-disable-next-line
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+// eslint-disable-next-line
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 }) || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
